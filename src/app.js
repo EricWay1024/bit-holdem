256a608689cf0680281459dd58c28bcb1878a7ca
ericway1024 <ericway1024@gmail.com>
2023-02-11 21:06:54 +0000

    Update: Index page

--- src/app.js
+++ src/app.js
@@ -22,6 +22,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(3010, () => {
+  console.log('listening on *:3010');
 });
